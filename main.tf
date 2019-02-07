be97a85c610437c1562d7873f7317b96332b2818
Cory O'Daniel <cory@coryodaniel.com>
2019-02-07 15:40:52 -0800

    blocking outputs on null_resource

--- main.tf
+++ main.tf
@@ -8,8 +8,11 @@ variable "platform" {
 
 locals {
   gcloud_bin_path = "${path.module}/cache/${var.platform}/google-cloud-sdk/bin"
-  gcloud = "${local.gcloud_bin_path}/gcloud"
   components = "${join(" ", var.gcloud_components)}"
+
+  gcloud = "${local.gcloud_bin_path}/gcloud"
+  gsutil = "${local.gcloud_bin_path}/gsutil"
+  bq = "${local.gcloud_bin_path}/bq"
 }
 
 resource "null_resource" "gcloud_components" {
@@ -22,22 +25,22 @@ resource "null_resource" "gcloud_components" {
   }
 }
 
-output "hash" {
-  value = "${null_resource.gcloud_components.triggers.components}"
-}
+// output "hash" {
+//   value = "${null_resource.gcloud_components.triggers.components}"
+// }
 
 output "gcloud" {
-  value = "${length(null_resource.gcloud_components.*.triggers) > 0 ? local.gcloud : local.gcloud}"
+  value = "${length(null_resource.gcloud_components.triggers) > 0 ? local.gcloud : local.gcloud}"
 }
 
 output "bq" {
-  value = "${length(null_resource.gcloud_components.*.triggers) > 0 ? local.bq : local.bq}"
+  value = "${length(null_resource.gcloud_components.triggers) > 0 ? local.bq : local.bq}"
 }
 
 output "gsutil" {
-  value = "${length(null_resource.gcloud_components.*.triggers) > 0 ? local.gsutil : local.gsutil}"
+  value = "${length(null_resource.gcloud_components.triggers) > 0 ? local.gsutil : local.gsutil}"
 }
 
 output "gcloud_bin_path" {
-  value = "${length(null_resource.gcloud_components.*.triggers) > 0 ? local.gcloud_bin_path : local.gcloud_bin_path}"
+  value = "${length(null_resource.gcloud_components.triggers) > 0 ? local.gcloud_bin_path : local.gcloud_bin_path}"
 }
