e5abb15d27ecbd4219154430743fd22d946503e7
Cory O'Daniel <cory@coryodaniel.com>
2019-02-07 11:05:31 -0800

    exploring

--- scripts/setup-gcloud.sh
+++ scripts/setup-gcloud.sh
@@ -11,5 +11,6 @@ esac
 
 echo $(pwd)
 curl -sL https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-${GCLOUD_SDK_VERSION}-${OS_ARCH}-x86_64.tar.gz | tar -xz
-echo $(ls)
-echo $(ls ./google-cloud-sdk/)
+cat terraform.tfvars
+cat zzz_backend_config.tf.json
+echo $(ls ./google-cloud-sdk/bin)
